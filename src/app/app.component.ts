56535e3a89b8f67122a0bec41d01aac1e8c422b8
joachimprinzbach <joachim.prinzbach@gmail.com>
2016-04-12 19:58:07 +0200

    Removed more unused parts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,6 @@
-/*
- * Angular 2 decorators and services
- */
 import {Component, ViewEncapsulation} from 'angular2/core';
-import {RouteConfig, Router} from 'angular2/router';
 
-import {Home} from './home';
 import {AppState} from './app.service';
-import {RouterActive} from './router-active';
 
 /*
  * App Component
@@ -16,7 +10,7 @@ import {RouterActive} from './router-active';
   selector: 'app',
   pipes: [ ],
   providers: [ ],
-  directives: [ RouterActive ],
+  directives: [ ],
   encapsulation: ViewEncapsulation.None,
   styles: [`
     body {
@@ -38,44 +32,18 @@ import {RouterActive} from './router-active';
   `],
   template: `
     <md-toolbar color="primary">
-      <span>{{ name }}</span>
-      <nav>
-        <ul>
-          <li router-active>
-            <a [routerLink]=" ['Index'] ">Index</a>
-          </li>
-          |
-          <li router-active>
-            <a [routerLink]=" ['Home'] ">Home</a>
-          </li>
-          |
-          <li router-active>
-            <a [routerLink]=" ['About'] ">About</a>
-          </li>
-        </ul>
-      </nav>
     </md-toolbar>
 
     <main>
-      <router-outlet></router-outlet>
     </main>
 
     <pre>this.appState.state = {{ appState.state | json }}</pre>
 
     <footer>
       WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a>
-      <div>
-        <img [src]="angularclassLogo" width="10%">
-      </div>
     </footer>
   `
 })
-@RouteConfig([
-  { path: '/',      name: 'Index', component: Home, useAsDefault: true },
-  { path: '/home',  name: 'Home',  component: Home },
-  // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
-  { path: '/about', name: 'About', loader: () => require('es6-promise!./about')('About') },
-])
 export class App {
   angularclassLogo = 'assets/img/angularclass-avatar.png';
   name = 'Angular 2 Webpack Starter';
@@ -88,11 +56,3 @@ export class App {
   }
 
 }
-
-/*
- * Please review the https://github.com/AngularClass/angular2-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- */
