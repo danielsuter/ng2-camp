2907b57f637382c46361dd8ea862c038e1264b3d
danielsuter <suter_daniel@hotmail.com>
2016-04-19 17:36:49 +0200

    Implemented CRUD methods.

--- backend/src/main/java/ch/zuehlke/campplanner/controller/HotelController.java
+++ backend/src/main/java/ch/zuehlke/campplanner/controller/HotelController.java
@@ -4,7 +4,9 @@ import ch.zuehlke.campplanner.dao.HotelRepository;
 import ch.zuehlke.campplanner.domain.Hotel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.LinkedList;
@@ -25,9 +27,20 @@ public class HotelController {
         return hotels;
     }
 
-    // by id
-    // alle
-    // add
-    // update
-    // remove
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Hotel get(@PathVariable("id") Long id) {
+        return hotelRepository.findOne(id);
+    }
+
+    @Transactional
+    @RequestMapping(method = RequestMethod.POST)
+    public void addOrUpdate(Hotel hotel) {
+        hotelRepository.save(hotel);
+    }
+
+    @Transactional
+    @RequestMapping(path = "/{id}",method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") Long id) {
+        hotelRepository.delete(id);
+    }
 }
