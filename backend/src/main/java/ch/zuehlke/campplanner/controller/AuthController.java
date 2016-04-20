d7bafd74065dac69ddde8e9d7ab0c02286082b8b
danielsuter <suter_daniel@hotmail.com>
2016-04-20 17:13:50 +0200

    Added spring security for basic auth and in memory auth. Also added an example to demonstrate how to check the user name.

---
+++ backend/src/main/java/ch/zuehlke/campplanner/controller/AuthController.java
@@ -0,0 +1,33 @@
+package ch.zuehlke.campplanner.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/rest/auth")
+public class AuthController {
+
+    @RequestMapping(value = "/whoami", method = RequestMethod.GET)
+    public String whoami() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return user.getUsername();
+    }
+
+    @RequestMapping(path="/logout", method = RequestMethod.GET)
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        Authentication auth2 = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(auth2);
+    }
+}
