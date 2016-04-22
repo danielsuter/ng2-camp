03dd8ef55e56e43bc1793349dba1a7975a292aa7
danielsuter <suter_daniel@hotmail.com>
2016-04-22 11:25:45 +0200

    Use dev profile for integration tests.

--- backend/src/test/java/ch/zuehlke/campplanner/CampplannerApplicationTests.java
+++ backend/src/test/java/ch/zuehlke/campplanner/CampplannerApplicationTests.java
@@ -3,10 +3,12 @@ package ch.zuehlke.campplanner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("dev")
 @SpringApplicationConfiguration(classes = CampplannerApplication.class)
 @WebAppConfiguration
 public class CampplannerApplicationTests {
