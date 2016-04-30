237cd1973e29181c46767b89d15af945217b5a8b
joachimprinzbach <joachim.prinzbach@gmail.com>
2016-04-30 11:31:54 +0200

    (fix) Minor refactoring of service and package structure. Removed sorting remporarily.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,41 +1,6 @@
 import {Component, ViewEncapsulation} from 'angular2/core';
-import {HotelsComponent} from './hotels.component';
+import {HotelsComponent} from './hotel-overview/hotels.component.ts';
 import {MaterializeDirective} from 'angular2-materialize';
-
-export class Hotel {
-  public id: number;
-  public name: string;
-  public description: string;
-  public zipCode: string;
-  public city: string;
-  public countryCode: string;
-  public website: string;
-  public tripAdvisorUrl: string;
-  public holidayCheckUrl: string;
-  public rooms: number;
-
-  constructor(id: number, name: string, description: string, zipCode: string, city: string, countryCode: string, website: string, tripAdvisorUrl: string, holidayCheckUrl: string, rooms: number) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.zipCode = zipCode;
-    this.city = city;
-    this.countryCode = countryCode;
-    this.website = website;
-    this.tripAdvisorUrl = tripAdvisorUrl;
-    this.holidayCheckUrl = holidayCheckUrl;
-    this.rooms = rooms;
-  }
-
-  domain(): string {
-    try {
-      const link: string = this.website.split('//')[1];
-      return link.split('/')[0];
-    } catch (err) {
-      return undefined;
-    }
-  }
-}
 
 @Component({
   selector: 'app',
---
+++ frontend/src/app/services/hotel.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from 'angular2/core';
+import {Http, Response} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+import {Hotel} from "./../model/hotel.model.ts";
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class HotelService {
+
+  constructor(private http: Http) {
+  }
+
+  getHotels(): Observable<Hotel[]> {
+    //const queryUrl: string = `http://localhost:8080/rest/hotels`;
+    const queryUrl: string = `http://beta.json-generator.com/api/json/get/NkpdDDTeb`;
+    return this.http.get(queryUrl)
+      .map((res:Response) => res.json());
+  }
+}
