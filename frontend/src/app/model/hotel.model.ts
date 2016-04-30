b26ec3d42bf6010358a7894f0c812ac59e43fbaa
brookman <rc-p90@bluemail.ch>
2016-04-30 12:32:14 +0200

    Frontent: Linting

--- frontend/src/app/model/hotel.model.ts
+++ frontend/src/app/model/hotel.model.ts
@@ -1,14 +1,14 @@
 export interface Hotel {
-  id:number;
-  name:string;
-  description:string;
-  zipCode:string;
-  city:string;
-  countryCode:string;
-  website:string;
-  tripAdvisorUrl:string;
-  holidayCheckUrl:string;
-  rooms:number;
+  id: number;
+  name: string;
+  description: string;
+  zipCode: string;
+  city: string;
+  countryCode: string;
+  website: string;
+  tripAdvisorUrl: string;
+  holidayCheckUrl: string;
+  rooms: number;
 }
 
 // http://beta.json-generator.com/NkpdDDTeb
