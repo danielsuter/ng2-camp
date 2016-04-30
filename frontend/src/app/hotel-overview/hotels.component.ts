4f5e1edfb1449aa5acf3729d64db50f50d6f6729
joachimprinzbach <joachim.prinzbach@gmail.com>
2016-04-30 11:35:37 +0200

    fixed some warnings.

--- frontend/src/app/hotel-overview/hotels.component.ts
+++ frontend/src/app/hotel-overview/hotels.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
 import {HotelService} from './../services/hotel.service.ts';
 import {Observable} from 'rxjs/Observable';
-import {Hotel} from "./../model/hotel.model.ts";
+import {Hotel} from './../model/hotel.model.ts';
 
 @Component({
   selector: 'hotels',
--- frontend/src/app/services/hotel.service.ts
+++ frontend/src/app/services/hotel.service.ts
@@ -1,8 +1,7 @@
 import {Injectable} from 'angular2/core';
 import {Http, Response} from 'angular2/http';
 import {Observable} from 'rxjs/Observable';
-import {Hotel} from "./../model/hotel.model.ts";
-import 'rxjs/add/operator/map';
+import {Hotel} from './../model/hotel.model.ts';
 
 @Injectable()
 export class HotelService {
@@ -11,9 +10,9 @@ export class HotelService {
   }
 
   getHotels(): Observable<Hotel[]> {
-    //const queryUrl: string = `http://localhost:8080/rest/hotels`;
+    // const queryUrl: string = `http://localhost:8080/rest/hotels`;
     const queryUrl: string = `http://beta.json-generator.com/api/json/get/NkpdDDTeb`;
     return this.http.get(queryUrl)
-      .map((res:Response) => res.json());
+      .map((res: Response) => res.json());
   }
 }
