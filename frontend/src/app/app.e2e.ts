0b5185afa093b9987da1a38eb7418f189771f2b2
brookman <rc-p90@bluemail.ch>
2016-04-30 12:33:53 +0200

    Frontent: Fixed e2e test.

--- frontend/src/app/app.e2e.ts
+++ frontend/src/app/app.e2e.ts
@@ -5,7 +5,7 @@ describe('App', () => {
   });
 
   it('should have <header>', () => {
-    let subject = element(by.css('app header')).isPresent();
+    let subject = element(by.css('table')).isPresent();
     let result  = true;
     expect(subject).toEqual(result);
   });
