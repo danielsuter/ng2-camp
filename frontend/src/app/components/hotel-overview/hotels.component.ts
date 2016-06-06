f9b5a747af058d5a7caf28a2cda0003594f45e4b
Magda <Magdalena.Sadlo@zuehlke.com>
2016-06-06 15:37:41 +0200

    filtering (ui) - first draft

--- frontend/src/app/components/hotel-overview/hotels.component.ts
+++ frontend/src/app/components/hotel-overview/hotels.component.ts
@@ -3,21 +3,38 @@ import {Observable} from 'rxjs/Observable';
 import {HotelService} from './../../shared/hotel.service.ts';
 import {Hotel} from '../../model/backend-typings';
 import FilterPipe from '../hotel.filter.pipe.ts';
+import {MaterializeDirective} from "angular2-materialize/dist/index";
 
 @Component({
   selector: 'hotels',
-  directives: [],
+  directives: [MaterializeDirective],
   providers: [HotelService],
   pipes: [FilterPipe],
   template: require('./hotels.component.html')
 })
 export class HotelsComponent implements OnInit {
   hotels: Observable<Hotel[]>;
+  countries = [];
+  private selectedValues =[];
 
   constructor(private hotelService: HotelService) {
   }
 
   ngOnInit() {
     this.hotels = this.hotelService.getHotelsAuthenticated();
+    this.countries = [
+      {value:"CH",name:"Switzerland"},
+      {value:"DE",name:"Germany"},
+      {value:"AT",name:"Austria"}
+    ];
+
+  }
+
+  change(options) {
+    this.selectedValues = Array.apply(null, options)  // convert to real Array
+      .filter(option => option.selected)
+      .map(option => option.value);
+
+    console.log(this.selectedValues);
   }
 }
