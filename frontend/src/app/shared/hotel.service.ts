4b27dafb430466c1d36edc89cb3dbbc7ad005d45
brookman <rc-p90@bluemail.ch>
2016-06-06 17:01:00 +0200

    frondend: cleaned up hotel service

--- frontend/src/app/shared/hotel.service.ts
+++ frontend/src/app/shared/hotel.service.ts
@@ -2,15 +2,13 @@ import {Injectable} from '@angular/core';
 import {Response} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import {Router} from '@ngrx/router';
-import {AuthHttp} from 'angular2-jwt';
 import {Hotel} from '../model/backend-typings';
 import {CrudService} from './crud.service';
 
 @Injectable()
 export class HotelService {
 
-  constructor(private authHttp: AuthHttp, private router: Router, private crud: CrudService) {
+  constructor(private crud: CrudService) {
   }
 
   saveHotel(hotel: Hotel): Observable<Hotel> {
@@ -27,18 +25,5 @@ export class HotelService {
 
   deleteHotel(id: number): Observable<Response> {
     return this.crud.delete('/rest/hotels/' + id);
-  }
-
-  private extractData(res: Response) {
-    let body = res.json();
-    return body || {};
-  }
-
-  private handleError(error: Error) {
-    console.log('error', error);
-    if (error.message === 'No JWT present') {
-      console.log('no JWT');
-      this.router.go('/login');
-    }
   }
 }
