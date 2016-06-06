af7881beac599c1b45cf190dba477fe5855f5ffa
joachimprinzbach <joachim.prinzbach@gmail.com>
2016-06-06 14:07:12 +0200

    Add testDataGenerator to generate hotels to be used in unit tests. Add unit test for hotelFilterPipe.

---
+++ frontend/src/app/components/hotel.filter.pipe.spec.ts
@@ -0,0 +1,25 @@
+import HotelFilterPipe from './hotel.filter.pipe';
+import {Hotel} from '../model/backend-typings';
+import {TestDataGenerator} from '../model/test.data.generator';
+
+describe('HotelFilterPipe', () => {
+  let pipe: HotelFilterPipe;
+  let hotels:Hotel[] = TestDataGenerator.generateHotels();
+  let filter:string = "";
+
+  beforeEach(() => {
+    pipe = new HotelFilterPipe();
+  });
+
+  it('filters the hotels list by name and city', () => {
+    filter = "Schluch";
+    let filteredAmount:number = 2;
+    expect(pipe.transform(hotels, filter).length).toEqual(filteredAmount);
+  });
+
+  it('filters the hotels list by name and city and ignores cases', () => {
+    filter = "rItTEr";
+    let filteredAmount:number = 2;
+    expect(pipe.transform(hotels, filter).length).toEqual(filteredAmount);
+  });
+});
