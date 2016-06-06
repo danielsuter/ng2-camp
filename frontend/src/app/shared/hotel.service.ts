6745ada5a98db46c9aad67e767e28a63380c7016
joachimprinzbach <joachim.prinzbach@gmail.com>
2016-06-06 16:01:43 +0200

    remove http dependency from service.

--- frontend/src/app/shared/hotel.service.ts
+++ frontend/src/app/shared/hotel.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {Response, Headers, RequestOptions} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import {Subject} from 'rxjs/Subject';
 import 'rxjs/add/operator/map';
