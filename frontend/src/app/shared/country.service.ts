33a8f3317ee8af823f21e919cc31eb6ff98a0e96
brookman <rc-p90@bluemail.ch>
2016-06-06 22:58:16 +0200

    frontend: simplified country service

--- frontend/src/app/shared/country.service.ts
+++ frontend/src/app/shared/country.service.ts
@@ -1,45 +1,15 @@
 import {Injectable} from '@angular/core';
-import {Headers, Response} from '@angular/http';
-import {AuthHttp} from "angular2-jwt";
-import {UrlProvider} from "./urlProvider";
-import {Observable} from "rxjs/Observable";
-import {Country} from "../model/backend-typings";
-import {Subject} from "rxjs/Subject";
-import {Router} from '@ngrx/router';
+import {Observable} from 'rxjs/Observable';
+import {Country} from '../model/backend-typings';
+import {CrudService} from './crud.service';
 
 @Injectable()
 export class CountryService {
 
-  constructor(
-    private authHttp: AuthHttp,
-    private router: Router
-  ) { }
-
-  getCountriesAuthenticated() : Observable<Country[]> {
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-
-    let countriesSubject = new Subject<Country[]>();
-
-    this.authHttp.get(UrlProvider.getBackendUrl('/rest/countries'), { headers: headers })
-      .subscribe(
-        value => countriesSubject.next(value.json()),
-        error => this.handleError(error),
-        () => countriesSubject.complete()
-      );
-    return countriesSubject.asObservable();
+  constructor(private crud: CrudService) {
   }
 
-  private handleError(error: Error) {
-    console.log('error', error);
-    if (error.message === 'No JWT present') {
-      console.log('no JWT');
-      this.router.go('/login');
-    }
-  }
-
-  private extractData(res: Response) {
-    let body = res.json();
-    return body || { };
+  getCountriesAuthenticated(): Observable<Country[]> {
+    return this.crud.get('/rest/countries');
   }
 }
