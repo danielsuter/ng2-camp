2b39589bfd29b986cfc3a3307cbdfbc19582e284
brookman <rc-p90@bluemail.ch>
2016-06-06 16:05:36 +0200

    frondend: cleaned up routers

--- frontend/src/app/routes/routes.ts
+++ frontend/src/app/routes/routes.ts
@@ -1,12 +1,11 @@
 import {Routes, Route} from '@ngrx/router';
-import {HomeComponent} from '../components/home/home.component.ts';
 import {HotelsComponent} from '../components/hotel-overview/hotels.component';
 import {HotelDetailComponent} from '../components/hotel-detail/hotel-detail.component';
 import {LoginComponent} from '../components/login/login.component';
 import {AuthGuard} from './authGuard';
-import {HotelNewComponent} from "../components/hotel-new/hotel-new.component";
+import {HotelNewComponent} from '../components/hotel-new/hotel-new.component';
 
-export const routes:Routes = [
+export const routes: Routes = [
   {
     path: '/',
     component: HotelsComponent
@@ -22,11 +21,11 @@ export const routes:Routes = [
   },
   {
     path: '/hotels/:id',
-    guards: [ AuthGuard ],
+    guards: [AuthGuard],
     component: HotelDetailComponent
   },
   {
     path: '/login',
     component: LoginComponent
   }
-];
+] as Route[];
